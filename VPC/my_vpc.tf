ecfdeeda7a37a463642af6cb78acc82d04f36699
Fokoue22 <fokouethomasdylan@gmail.com>
2023-04-28 11:58:57 +0100

    first change

--- VPC/my_vpc.tf
+++ VPC/my_vpc.tf
@@ -1,1 +1,25 @@
 
+# Terraform block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0" # this version will download aws v4.0-4.9
+    }
+  }
+}
+
+#this is the provider
+provider "aws" {
+  region = "us-west-2"
+  
+}
+
+# Deploy an ec2 instance 
+resource "aws_vpc" "my-ec2" {
+  #Argument 
+  cidr_block = "10.0.0.0/16"
+   tags = { 
+    Name = "my-vpc"
+  }
+}
